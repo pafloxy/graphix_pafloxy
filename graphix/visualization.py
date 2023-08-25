266f48f41384d9b2157f552a5b207714a849e401
pafloxy <61856561+pafloxy@users.noreply.github.com>
2023-08-25 16:03:02 +0530

    update scratch files

--- graphix/visualization.py
+++ graphix/visualization.py
@@ -64,6 +64,7 @@ class GraphVisualizer:
                 print("No flow or gflow found.")
                 nx.draw(self.g, with_labels=True)
                 plt.show()
+        return f, l_k
 
     def visualize_w_flow(self, f, l_k, angles=None, local_clifford=None, figsize=None, save=False, filename=None):
         """
@@ -181,12 +182,102 @@ class GraphVisualizer:
         if save:
             plt.savefig(filename)
 
-    def visualize_w_gflow(self, g, l_k, figsize=None):
+    def visualize_w_gflow(self, g, l_k, angles=None, local_clifford=None, figsize=None, save=False, filename=None):
         """
         visualizes the graph with gflow structure.
         to be implemented
         """
-        pass
+        if figsize is None:
+            width = (max(l_k.values()) + 1) * 0.8
+            height = len(self.v_in)
+            figsize = (width, height)
+        plt.figure(figsize=figsize)
+        # pos = nx.spring_layout(self.g)  # Initial layout for the nodes
+
+        # n = len(self.v_in)
+        # for i in range(n):
+        #     k = self.v_in[i]
+        #     pos[k][1] = i
+        #     while k in f.keys():
+        #         k = f[k]
+        #         pos[k][1] = i
+        layer_data = process_layer_data(self.g, l_k)
+        nx.set_node_attributes(self.g, layer_data['attribute_data'])
+        pos = nx.multipartite_layout(self.g, subset_key= 'layer', align= 'vertical')
+
+        # Change the x coordinates of the nodes based on their layer, sort in descending order
+        for node, layer in l_k.items():
+            pos[node] = (max(l_k.values()) - layer, pos[node][1])  # Subtracting from max for descending order
+
+        # Draw the arrows
+        for a, b in g.items():
+            nx.draw_networkx_edges(self.g, pos, edgelist=[(a, c) for c in b], edge_color="black", arrowstyle="->", arrows=True)
+
+        # Draw the dashed edges
+        for edge in self.g.edges():
+            if edge[1] not in g.get(edge[0]) and edge[0] not in g.get(edge[1]) :  # This edge is not an arrow
+                intersect = False
+                bezier_path = [pos[edge[0]]]
+                for node in self.g.nodes():
+                    if (
+                        node != edge[0]
+                        and node != edge[1]
+                        and self.edge_intersects_node(pos[edge[0]], pos[edge[1]], pos[node])
+                    ):
+                        intersect = True
+                        ctrl_point = self.control_point(pos[edge[0]], pos[edge[1]], pos[node])
+                        bezier_path.append(ctrl_point)
+                if not intersect:
+                    nx.draw_networkx_edges(self.g, pos, edgelist=[edge], style="dashed", alpha=0.7)
+                    continue
+                bezier_path.append(pos[edge[1]])
+                t = np.linspace(0, 1, 100)
+                curve = self.bezier_curve(bezier_path, t)
+                plt.plot(curve[:, 0], curve[:, 1], "k--", linewidth=1, alpha=0.7)
+
+        # Draw the nodes with different colors based on their role (input, output, or other)
+        for node in self.g.nodes():
+            color = "black"  # default color for 'other' nodes
+            inner_color = "white"
+            if node in self.v_in:
+                color = "red"
+            if node in self.v_out:
+                inner_color = "lightgray"
+            elif angles is not None and (angles[node] == 0 or angles[node] == 1 / 2):
+                inner_color = "lightblue"
+            plt.scatter(
+                *pos[node], edgecolor=color, facecolor=inner_color, s=350, zorder=2
+            )  # Draw the nodes manually with scatter()
+
+        if local_clifford is not None:
+            for node in self.g.nodes():
+                if node in local_clifford.keys():
+                    plt.text(*pos[node] + np.array([0.2, 0.2]), f"{local_clifford[node]}", fontsize=10, zorder=3)
+
+        # Draw the labels
+        nx.draw_networkx_labels(self.g, pos)
+
+        x_min = min([pos[node][0] for node in self.g.nodes()])  # Get the minimum x coordinate
+        x_max = max([pos[node][0] for node in self.g.nodes()])  # Get the maximum x coordinate
+        y_min = min([pos[node][1] for node in self.g.nodes()])  # Get the minimum y coordinate
+        y_max = max([pos[node][1] for node in self.g.nodes()])  # Get the maximum y coordinate
+
+        # Draw the vertical lines to separate different layers
+        for layer in range(min(l_k.values()), max(l_k.values())):
+            plt.axvline(x=layer + 0.5, color="gray", linestyle="--", alpha=0.5)  # Draw line between layers
+        for layer in range(min(l_k.values()), max(l_k.values()) + 1):
+            # plt.axvline(x=layer + 0.5, color='gray', linestyle='--')  # Draw line between layers
+            plt.text(
+                layer, y_min - 0.5, f"l: {max(l_k.values()) - layer}", ha="center", va="top"
+            )  # Add layer label at bottom
+
+        plt.xlim(x_min - 0.5, x_max + 0.5)  # Add some padding to the left and right
+        plt.ylim(y_min - 1, y_max + 0.5)  # Add some padding to the top and bottom
+        plt.show()
+
+        if save:
+            plt.savefig(filename)
+
 
     @staticmethod
     def edge_intersects_node(start, end, node_pos, buffer=0.2):
@@ -225,3 +316,15 @@ class GraphVisualizer:
 
 def comb(n, r):
     return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
+
+def process_layer_data(graph: nx.Graph, layer_map: dict):
+
+    node_attributes = {}; max_l = max(set(layer_map.values()))
+    layer_data = {n: set() for n in range(max_l +1 )}
+
+    for v in graph.nodes():
+        # print(v); print(layer_data[v])
+        node_attributes[v] = {"layer" : layer_map[v]}
+        layer_data[layer_map[v]].add(v)
+
+    return {'attribute_data' : node_attributes, 'nodes_in_layers': layer_data, 'num_layers' : max_l  }
