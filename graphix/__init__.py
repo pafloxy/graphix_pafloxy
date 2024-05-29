4b9c95a5b365e86e4aeb30a319ae9e1e77c84570
pafloxy <rajarsi14p@gmail.com>
2024-05-29 15:02:38 +0200

    update 29.05.24.15.02

--- graphix/__init__.py
+++ graphix/__init__.py
@@ -3,3 +3,4 @@ from graphix.graphsim import GraphState
 from graphix.pattern import Pattern
 from graphix.sim.statevec import Statevec
 from graphix.transpiler import Circuit
+from graphix.parameter import Parameter
